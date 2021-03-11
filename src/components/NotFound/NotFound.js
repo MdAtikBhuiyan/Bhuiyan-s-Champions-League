91aef0794e99bf0bbf48646108a8ba322f95d56b
MdAtikBhuiyan <touhidatik81@gmail.com>
2021-03-11 23:37:17 +0600

    assignment-done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='container'>
-            <h1 className='text-center text-danger my-5'> Sorry!! error not found </h1>
+            <h1 className='text-center text-danger my-5'> ERROR !! not found </h1>
         </div>
     );
 };
